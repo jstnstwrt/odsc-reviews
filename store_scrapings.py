d657588bca4b82ed4bb7ed37c91f0d40e739ddbe
Justin Stewart <justinstewart.la@gmail.com>
2015-09-02 15:05:00 -0400

    pushin up files

--- store_scrapings.py
+++ store_scrapings.py
@@ -12,7 +12,9 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-for i in range(50,100):
+
+
+for i in range(1,2):
     url = "http://www.theeroticreview.com/discussion_boards/messageList.asp?boardID=12&page=" + str(i)
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "lxml")
@@ -33,15 +35,24 @@ for i in range(50,100):
     	except: 
     		print 'still trying'
 
-        
-        # try:
-        #     post_data = post.find_all('strong')
-            # username = str(post_data[0])[len('<strong>'):].strip('</strong>')
-            # timestamp = str(post_data[1])[len('<strong>'):].strip('</strong>')
-        #     post = UserPost(username = username, timestamp = timestamp)
-        #     
-        # except IndexError:
-        #     nonsense_data.append(post_data)
-        #     print 'CRAPPY HTML!'
+    
+# for i in range(50,100):
+#     url = "http://www.theeroticreview.com/discussion_boards/messageList.asp?boardID=12&page=" + str(i)
+#     r = requests.get(url)
+#     soup = BeautifulSoup(r.content, "lxml")
+    
+#     discussion_board_data = soup.find("div", { "class" : "content-layout thread-tree-paginated" })
+#     posts =  discussion_board_data.find_all('li')
+   
+#     # nonsense_data = []
 
-
+#     for post in posts:
+#       post_data = post.find_all('strong')
+#       try:
+#           username = str(post_data[0])[len('<strong>'):].strip('</strong>')
+#           post = UserPost(username = username)
+#           session.add(post)
+#           session.commit()
+#           print 'added post!'
+#       except: 
+#           print 'still trying'
