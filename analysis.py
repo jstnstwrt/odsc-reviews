adee1cf5e961dcc7020cd0b3e8164d79fd03bced
hugobowne <hugobowne@gmail.com>
2015-09-02 16:29:09 -0400

    script now reads sql db and plots histogram of users use

--- analysis.py
+++ analysis.py
@@ -1,14 +1,37 @@
 import sqlite3
 import pandas as pd
-
-import matplotlib as plt
+import os
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 import numpy as np
 # matplotlib.style.use('ggplot')
 
-con = sqlite3.connect("user_frequency.db")
-df = pd.read_sql_query("select * from username_counts", con)
 
-plt.()
+BASE_DIR = os.path.dirname(os.path.abspath('/Users/hugobowne-anderson/repos/odsc-erotic-review'))
+db_path = os.path.join(BASE_DIR, "odsc-erotic-review/user_frequency.db")
+conn = sqlite3.connect(db_path)
 
 
-con.close()
+df = pd.read_sql_query("select * from username_counts", conn)
+
+df = df.rename(columns={'username': 'name', 'count(username)': 'number'})
+df = df.sort('number' , ascending = 0);
+df.head()
+
+
+N = 75
+
+ind = np.arange(N)    # the x locations for the groups
+width = 0.7       # the width of the bars: can also be len(x) sequence
+
+p1 = plt.bar(ind, df.number[0:N],   width, color='r')
+
+
+plt.ylabel('Number of posts/comments')
+plt.title('People who say things that interest us (top ' + str(N)+')')
+#plt.xticks(ind+width/2., list(df.name[0:N]) , rotation='vertical' )
+plt.xticks(ind+width/2., range(1,N+1)  )
+#plt.yticks(np.arange(0,81,10))
+#plt.legend( (p1[0], p2[0]), ('Men', 'Women') )
+
+plt.show()
