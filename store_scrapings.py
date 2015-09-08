10e1f830885c65cf4587e95d19f2eb54222af17f
Justin Stewart <justinstewart.la@gmail.com>
2015-09-08 15:03:39 -0400

    added commments to store scrapings

--- store_scrapings.py
+++ store_scrapings.py
@@ -1,34 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, UserPost
 
-import requests
-from bs4 import BeautifulSoup
-import re
 
+# Creating the database connection.
 engine = create_engine('sqlite:///user_frequency.db')
-
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-
-
+# Interating over the pages of the forum.
 for i in range(1,101):
-    url = "http://www.theeroticreview.com/discussion_boards/messageList.asp?boardID=12&page=" + str(i)
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "lxml")
-    
-    discussion_board_data = soup.find("div", { "class" : "content-layout thread-tree-paginated" })
+    # Setting the base url of the general discussion forum.
+    url = "http://www.theeroticreview.com/"\
+          "discussion_boards/messageList.asp?boardID=12&page=" + str(i)
+    r = requests.get(url) # Grabbing the source code of the webpage.
+    soup = BeautifulSoup(r.content, "lxml") # Parsing the html.
+    # Identifying the key element containing all the posts. 
+    discussion_board_data = soup.find(
+        "div", 
+        { "class" : "content-layout thread-tree-paginated" })
+    # Creating a list of all the list elements. 
+    # Each list element is contains one post.
     posts =  discussion_board_data.find_all('li')
    
     for post in posts:
+        # All usernames are contained in strong html tags. 
     	post_data = post.find_all('strong')
     	try:
     		username = str(post_data[0])[len('<strong>'):].strip('</strong>')
     		post = UserPost(username = username)
-    		# session.add(post)
-    		# session.commit()
-    		print 'added post!'
+            # Store username as an entry in the database. 
+    		session.add(post)
+    		session.commit()
+    		print 'added post to db!'
     	except: 
     		print 'some error'
