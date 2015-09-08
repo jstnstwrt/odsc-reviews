2786a16512c27719fa75e03709481a29f2dd23ac
Justin Stewart <justinstewart.la@gmail.com>
2015-09-08 12:39:54 -0400

    adding comments

--- store_scrapings.py
+++ store_scrapings.py
@@ -14,7 +14,7 @@ session = DBSession()
 
 
 
-for i in range(1,2):
+for i in range(1,101):
     url = "http://www.theeroticreview.com/discussion_boards/messageList.asp?boardID=12&page=" + str(i)
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "lxml")
@@ -22,37 +22,13 @@ for i in range(1,2):
     discussion_board_data = soup.find("div", { "class" : "content-layout thread-tree-paginated" })
     posts =  discussion_board_data.find_all('li')
    
-    # nonsense_data = []
-
     for post in posts:
     	post_data = post.find_all('strong')
     	try:
     		username = str(post_data[0])[len('<strong>'):].strip('</strong>')
     		post = UserPost(username = username)
-    		session.add(post)
-    		session.commit()
+    		# session.add(post)
+    		# session.commit()
     		print 'added post!'
     	except: 
-    		print 'still trying'
-
-    
-# for i in range(50,100):
-#     url = "http://www.theeroticreview.com/discussion_boards/messageList.asp?boardID=12&page=" + str(i)
-#     r = requests.get(url)
-#     soup = BeautifulSoup(r.content, "lxml")
-    
-#     discussion_board_data = soup.find("div", { "class" : "content-layout thread-tree-paginated" })
-#     posts =  discussion_board_data.find_all('li')
-   
-#     # nonsense_data = []
-
-#     for post in posts:
-#       post_data = post.find_all('strong')
-#       try:
-#           username = str(post_data[0])[len('<strong>'):].strip('</strong>')
-#           post = UserPost(username = username)
-#           session.add(post)
-#           session.commit()
-#           print 'added post!'
-#       except: 
-#           print 'still trying'
+    		print 'some error'
